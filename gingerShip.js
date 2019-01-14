7002097d7d2e14c7a536f8f85b278002491b2c53
Benjaminphan04 <bphan1@ocdsb.ca>
2019-01-14 13:53:22 -0500

    Create gingerShip.js

---
+++ gingerShip.js
@@ -0,0 +1,13 @@
+function gingerShip() {
+
+  this.x = width / 2;
+
+  this.show = function() {
+    fill(255)
+    image(Ginger, this.x, height - 90, 70, 90);
+  }
+
+  this.move = function(dir) {
+    this.x += dir * 10
+  }
+}
